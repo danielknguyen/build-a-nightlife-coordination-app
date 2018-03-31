03b315fbd2756c2a91e1464c8fe9b51f5b6a0b80
Daniel Nguyen <danielnguyen93@hotmail.com>
2018-03-30 17:44:30 -0700

    routes module boiler

---
+++ routes/routes.js
@@ -0,0 +1,9 @@
+var routes = function(app, flash, util, bcrypt, assert) {
+
+  app.get('/', function(req, res) {
+    // console.log(res.locals);
+    res.render('index.html', );
+  });
+
+}
+module.exports = routes;
